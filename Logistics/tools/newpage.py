3a43bb8b7d94ee229c9ea1b2e41c25b09960c1c5
zhaoxing <695806420@qq.com>
2020-03-09 13:30:17 +0800

    'page'

--- Logistics/tools/newpage.py
+++ Logistics/tools/newpage.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 # 分页
 class PageViewSet(PageNumberPagination):
     page_size = 10
-    max_page_size = 10
+    max_page_size = 100
     page_query_param = 'p'
     page_size_query_param = 'page_size'
 
